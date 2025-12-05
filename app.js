3f3cc589f6b4ac255bfd2fa1a60cb2666473713b
Yagiz <yagizdemirezen@gmail.com>
2025-12-05 20:37:53 +0300

    redirect added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const reservationsRouter = require('./src/routes/reservationRoutes');
-
+app.get('/', (req, res) => {
+    res.redirect('/api/reservations');
+});
 app.use(express.json());
 app.use('/api/reservations', reservationsRouter);
 
